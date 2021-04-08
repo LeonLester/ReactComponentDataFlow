7ac11ac3b2956efcb0d4a7aceede2b5c2c1ae3a1
pellul <paulinhoellul@gmail.com>
2021-04-08 15:59:13 +0300

    Added cousins flow

---
+++ pages/withContext/cousins/fatherLeft.js
@@ -0,0 +1,19 @@
+import ChildLeft from './childLeft';
+import {MyContext} from './grandFather';
+import 'antd/dist/antd.css';
+import {Row, Card} from 'antd';
+import {useContext} from 'react';
+
+export default function father() {
+    const context = useContext(MyContext);
+
+    return (
+        <div>
+            <Row>
+                <Card title="Left Father" extra={<h1>{context.state}</h1>} style={{width: "100%"}}>
+                    <ChildLeft/>
+                </Card>
+            </Row>
+        </div>
+    )
+}
---
+++ pages/withContext/cousins/fatherRight.js
@@ -0,0 +1,18 @@
+import ChildRight from './childRight';
+import {MyContext} from './grandFather';
+import 'antd/dist/antd.css';
+import {Row, Card} from 'antd';
+import {useContext} from 'react';
+
+export default function father() {
+    const context = useContext(MyContext);
+    return (
+        <div>
+            <Row>
+                <Card title="Right Father" extra={<h1>{context.state}</h1>} style={{width: "100%"}}>
+                    <ChildRight/>
+                </Card>
+            </Row>
+        </div>
+    )
+}
