bea1eba855a99aadd01614b1f60ee22b4f119b58
ApostolisHD <tolis1943@windowslive.com>
2021-04-08 11:26:03 +0300

    Added Form Tot grandFather

--- components/grandFather.js
+++ components/grandFather.js
@@ -1,20 +1,37 @@
 import Child from '../components/child'
 import {useState} from 'react'
 import Father from './father'
-import {Row, Col, Space, Card} from 'antd';
+import {Row, Col,Form,Card,Input,Button} from 'antd';
 import {Layout} from 'antd';
+import FormItem from 'antd/lib/form/FormItem';
 
 const {Header, Footer, Content} = Layout;
 
 export default function grandFather(props) {
   const [grandData,setGrandData] = useState("default")
   //console.log(props,"grandfather")
+  function onFinish(values) {
+    setGrandData(values.name)
+  }
+
   return (
     <div>
       <Row>
       <Col span={8}></Col>
         <Col span={8}>
         <Card title="Eimai O Pappous kai exw auth thn timi" extra={<h1>{grandData}</h1>} style={{ width: "100%"}}>
+        {props.downwards ? 
+            <Form onFinish={onFinish}>
+              <FormItem name="name">
+                <Input></Input>
+              </FormItem>
+              <FormItem>
+                <Button htmlType="submit">
+                  ok!
+                </Button>
+              </FormItem>
+            </Form>
+            :null}
         <Father
         data={grandData}
         downwards={props.downwards}
