648ac4de76be42ad6f4af7397629b133cabcc1e0
sphantom99 <georgeal1999@gmail.com>
2021-04-09 12:04:48 +0300

    Added comments to downwards direct flow

--- components/downwardsDirect/childDownwardsDirect.js
+++ components/downwardsDirect/childDownwardsDirect.js
@@ -1,6 +1,11 @@
 import 'antd/dist/antd.css';
 import {Row,Card} from 'antd';
 
+/*
+  This the Child component. Same as its father, it states that it will accept props (fatherProps which if you console.log will contain 
+  a data var with the grandData passed from its father. Once it receives the props it can use them however it likes. In this example 
+  we just view it in a span.
+*/
 export default function childDownwards(fatherProps) {
   return (
     <Row>
--- components/downwardsDirect/grandFatherDownwardsDirect.js
+++ components/downwardsDirect/grandFatherDownwardsDirect.js
@@ -1,10 +1,19 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import FatherDD from './fatherDownwardsDirect'
-import {Row, Col,Form,Card,Input,Button} from 'antd';
+import { Row, Col, Form, Card, Input, Button } from 'antd';
 import FormItem from 'antd/lib/form/FormItem';
 
+/*
+
+   This is the grandfather component. In the 'downwards direct' flow, data is passed through all of the components in the hierarchy.
+   That means that the data passes through every component 'below' the one that wishes to pass data. To use this method you simply 
+   need to pass the data in the component as shown below. Inside the <FatherDD> component we set a value called data to which we assign
+   the values we need to send downwards 
+
+*/
+
 export default function grandFatherDownwards() {
-  const [grandData,setGrandData] = useState("default")
+  const [grandData, setGrandData] = useState("default")
 
   function onFinish(values) {
     setGrandData(values.name)
@@ -13,26 +22,26 @@ export default function grandFatherDownwards() {
   return (
     <div>
       <Row>
-      <Col span={8}></Col>
+        <Col span={8}></Col>
         <Col span={8}>
-        <Card title="I am GrandFather:" extra={<h1>{grandData}</h1>} style={{ width: "100%"}}>
+          <Card title="I am GrandFather:" extra={<h1>{grandData}</h1>} style={{ width: "100%" }}>
 
-        <div id="form">
-            <Form onFinish={onFinish}>
-              <FormItem name="name">
-                <Input></Input>
-              </FormItem>
-              <FormItem>
-                <Button htmlType="submit">
-                  ok!
+            <div id="form">
+              <Form onFinish={onFinish}>
+                <FormItem name="name">
+                  <Input></Input>
+                </FormItem>
+                <FormItem>
+                  <Button htmlType="submit">
+                    ok!
                 </Button>
-              </FormItem>
-            </Form>
-        </div>
+                </FormItem>
+              </Form>
+            </div>
 
-        <FatherDD data={grandData}></FatherDD>
+            <FatherDD data={grandData}></FatherDD>
 
-      </Card>
+          </Card>
         </Col>
         <Col span={8}></Col>
         <Col span={8}></Col>
